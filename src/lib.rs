3c2dbfa17b5108f952e10621ad1d49c159ad2f20
Nathan Young <nathantayloryoung@gmail.com>
2024-05-25 17:59:17 +1200

    Mechanics for the battle system implemented

---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+trait Status {
+    fn inflict(&self, target: dyn Character);
+}
+
+pub trait Move {
+    fn execute(&self, target: dyn Character);
+}
+
+pub trait Character {
+    fn take_damage(&mut self, damage: u32);
+    fn change_stats(&mut self, stat: Stat, amount: u32, subtract: bool);
+}
+
+pub enum Stat {
+    Attack,
+    Defense,
+    Accuracy,
+    MaxHealth,
+}
+
+pub struct Player {
+    max_health: u32,
+    attack: u32,
+    defense: u32,
+    accuracy: u32,
+    current_health: u32,
+    possible_moves: Vec<Box<dyn Move>>,
+    status: Option<Box<dyn Status>>,
+}
+
+impl Player {
+    pub fn new(max_health: u32, possible_moves: Vec<Box<dyn Move>>) -> Self {
+        Player {
+            max_health,
+            attack: 0,
+            defense: 0,
+            accuracy: 0,
+            current_health: max_health,
+            possible_moves,
+            status: None,
+        }
+    }
+}
+
+impl Character for Player {
+    fn take_damage(&mut self, damage: u32) {
+        self.current_health -= damage;
+    }
+
+    fn change_stats(&mut self, stat: Stat, amount: u32, subtract: bool) {
+        let stat_to_change = match stat {
+            Stat::Attack => &mut self.attack,
+            Stat::Defense => &mut self.defense,
+            Stat::Accuracy => &mut self.accuracy,
+            Stat::MaxHealth => &mut self.max_health,
+        };
+        if subtract {
+            if amount > *stat_to_change {
+                *stat_to_change = 0;
+            } else {
+                *stat_to_change -= amount;
+            }
+        } else {
+            *stat_to_change += amount;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_stat_change() {
+        let mut player = Player::new(100, Vec::new());
+        player.change_stats(Stat::Attack, 10, false);
+        assert_eq!(player.attack, 10);
+        player.change_stats(Stat::Attack, 10, false);
+        assert_eq!(player.attack, 20);
+    }
+
+    #[test]
+    fn check_stat_change_subtract() {
+        let mut player = Player::new(100, Vec::new());
+        player.change_stats(Stat::Defense, 10, false);
+        assert_eq!(player.defense, 10);
+        player.change_stats(Stat::Defense, 10, true);
+        assert_eq!(player.defense, 0);
+    }
+
+    #[test]
+    fn check_stat_change_lower_invalid() {
+        let mut player = Player::new(100, Vec::new());
+        player.change_stats(Stat::Defense, 1, true);
+        assert_eq!(player.defense, 0);
+    }
+
+    #[test]
+    fn check_stat_change_lower() {
+        let mut player = Player::new(100, Vec::new());
+        player.change_stats(Stat::Defense, 0, true);
+        assert_eq!(player.defense, 0);
+    }
+}
